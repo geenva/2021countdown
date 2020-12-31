e3136f70d8e96f386db2921606ecdf6858611054
geenva <geenva@users.noreply.github.com>
2020-12-31 11:04:31 +0800

    ✨ windows version w/o emojis

---
+++ windows.js
@@ -0,0 +1,148 @@
+const cd = require("countdown");
+
+let c0 = setInterval(() => {
+  let countdown = cd(new Date(2021, 1, 1)).toString();
+  console.log(
+    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nTIME TILL NEW YEAR: " +
+      countdown.slice(9)
+  );
+  if (countdown.slice(9) == "1 second") {
+    setTimeout(() => {
+      process.exit;
+    }, 1800);
+    clearInterval(c0);
+    setTimeout(() => {
+      console.log(
+        "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHAPPY NEW YEAR!"
+      );
+      setTimeout(() => {
+        console.log("It's now 2021!");
+      }, 500);
+      setTimeout(() => {
+        console.log(
+          " .----------------.  .----------------.  .----------------.  .----------------. "
+        );
+      }, 1000);
+      setTimeout(() => {
+        console.log(
+          "| .--------------. || .--------------. || .--------------. || .--------------. |"
+        );
+      }, 1500);
+      setTimeout(() => {
+        console.log(
+          "| |    _____     | || |     ____     | || |    _____     | || |     __       | |"
+        );
+      }, 2000);
+      setTimeout(() => {
+        console.log(
+          "| |   / ___ `.   | || |   .'    '.   | || |   / ___ `.   | || |    /  |      | |"
+        );
+      }, 2500);
+      setTimeout(() => {
+        console.log(
+          "| |  |_/___) |   | || |  |  .--.  |  | || |  |_/___) |   | || |    `| |      | |"
+        );
+      }, 3000);
+      setTimeout(() => {
+        console.log(
+          "| |   .'____.'   | || |  | |    | |  | || |   .'____.'   | || |     | |      | |"
+        );
+      }, 3500);
+      setTimeout(() => {
+        console.log(
+          "| |  / /____     | || |  |  `--'  |  | || |  / /____     | || |    _| |_     | |"
+        );
+      }, 4000);
+      setTimeout(() => {
+        console.log(
+          "| |  |_______|   | || |   '.____.'   | || |  |_______|   | || |   |_____|    | |"
+        );
+      }, 4500);
+      setTimeout(() => {
+        console.log(
+          "| |              | || |              | || |              | || |              | |"
+        );
+      }, 5000);
+      setTimeout(() => {
+        console.log(
+          "| '--------------' || '--------------' || '--------------' || '--------------' |"
+        );
+      }, 5500);
+      setTimeout(() => {
+        console.log(
+          " '----------------'  '----------------'  '----------------'  '----------------' "
+        );
+      }, 6000);
+      setTimeout(() => {
+        console.log(
+          "🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉"
+        );
+      }, 6500);
+      setTimeout(() => {
+        console.log("HAPPY 2021!");
+      }, 7000);
+      setTimeout(() => {
+        setInterval(() => {
+          setTimeout(() => {
+            console.log(
+              " .----------------.  .----------------.  .----------------.  .----------------. "
+            );
+          }, 1000);
+          setTimeout(() => {
+            console.log(
+              "| .--------------. || .--------------. || .--------------. || .--------------. |"
+            );
+          }, 1500);
+          setTimeout(() => {
+            console.log(
+              "| |    _____     | || |     ____     | || |    _____     | || |     __       | |"
+            );
+          }, 2000);
+          setTimeout(() => {
+            console.log(
+              "| |   / ___ `.   | || |   .'    '.   | || |   / ___ `.   | || |    /  |      | |"
+            );
+          }, 2500);
+          setTimeout(() => {
+            console.log(
+              "| |  |_/___) |   | || |  |  .--.  |  | || |  |_/___) |   | || |    `| |      | |"
+            );
+          }, 3000);
+          setTimeout(() => {
+            console.log(
+              "| |   .'____.'   | || |  | |    | |  | || |   .'____.'   | || |     | |      | |"
+            );
+          }, 3500);
+          setTimeout(() => {
+            console.log(
+              "| |  / /____     | || |  |  `--'  |  | || |  / /____     | || |    _| |_     | |"
+            );
+          }, 4000);
+          setTimeout(() => {
+            console.log(
+              "| |  |_______|   | || |   '.____.'   | || |  |_______|   | || |   |_____|    | |"
+            );
+          }, 4500);
+          setTimeout(() => {
+            console.log(
+              "| |              | || |              | || |              | || |              | |"
+            );
+          }, 5000);
+          setTimeout(() => {
+            console.log(
+              "| '--------------' || '--------------' || '--------------' || '--------------' |"
+            );
+          }, 5500);
+          setTimeout(() => {
+            console.log(
+              " '----------------'  '----------------'  '----------------'  '----------------' \n\n\n"
+            );
+          }, 6000);
+        }, 7000);
+      }, 7000);
+
+      // setInterval exit
+    }, 1000);
+    // setTimeout exit
+  }
+}, 1000);
